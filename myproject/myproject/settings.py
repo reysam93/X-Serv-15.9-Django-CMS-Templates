0cf867b1b85da0dd133469e2b478bee21e5b4239
Samuel <samuel@samuel.(none)>
2015-04-17 19:07:00 +0200

    varios put sobre un recurso lo actualizan no duplican

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -81,6 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-TEMPLATE_DIRS = ('static/',)
+TEMPLATE_DIRS = ('Sundark/', 'static/')
 
 STATIC_URL = '/static/'
+STATIC_URL2 = 'Sundark'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -4,6 +4,8 @@ import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^annotated/static/(?P<path>.*)$', 'django.views.static.serve', 
+           {'document_root': settings.STATIC_URL2}),
     url(r'^logout$', 'django.contrib.auth.views.logout'),
     url(r'^annotated/(.*)$', 'cms_templates.views.withTemplates'),
     url(r'^(.*)$', 'cms_templates.views.processCmsRequest'),
